e41bb02b6d08c7c85d216c1d2c365eef3831b4b1
danchikhub <d.bakytov@yandex.com>
2020-04-24 20:45:00 +0600

    mini patch in index.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>LoveTravel</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="{{URL::asset('css\main.css')}}">
     
@@ -12,7 +12,7 @@
     <div class="header-section">
         <div class="container">
             <div class="header">
-                <a href="#" class="logo">LoveTravel</a>
+                <a href="/index" class="logo">LoveTravel</a>
                 <a href="{{ url('/') }}/login" class="come-in">Войти</a>
             </div>
             <div class="desc-web">
@@ -28,7 +28,7 @@
                     {{ csrf_field() }}
                     <div class="input-group">
                         <input type="text" class="form-control" name="q"
-                            placeholder="Search users"> <span class="input-group-btn">
+                            placeholder="Поиск города"> <span class="input-group-btn">
                             <button type="submit" class="btn btn-outline-success my-2 my-sm-0 btn-find-city">
                                 Поиск
                             </button>
