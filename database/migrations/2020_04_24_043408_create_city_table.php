a4cecf0d8cc6458d7350e63a613e6e3a727fbd5b
CypherShiro <36626333+ChypherShiro@users.noreply.github.com>
2020-04-24 10:50:00 +0600

    migrations and models

---
+++ database/migrations/2020_04_24_043408_create_city_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCityTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('city', function (Blueprint $table) {
+            $table->bigIncrements('id_city');//Id города
+            $table->string('name_city');//Название города
+            $table->string('opis_city');//Описание города
+            $table->string('img_city');//Картинка города
+            $table->integer('ejo');// Еда Жилье Отдых (1-еда 2-жилье 3-отдых)
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('city');
+    }
+}
---
+++ database/migrations/2020_04_24_043529_create_post_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->bigIncrements('id_post');//ID поста
+            $table->string('zag_post');//Заголовок поста
+            $table->string('opis_post');//Описание поста
+            $table->string('img_post');//Картинки поста
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}
