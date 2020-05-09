c848a8ab8263755950bf7396f3bd50db12c5c1b1
danchikhub <d.bakytov@yandex.com>
2020-05-09 23:50:00 +0600

    city

--- resources/views/city-test.blade.php
+++ resources/views/city-test.blade.php
@@ -5,11 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{URL::asset('css\main.css')}}">
 </head>
 <body>
+<div class="city-div pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+    <h1 class="name-city display-4"> {{$data->name_city}}</h1>
+    <p class="desc-city lead">{{$data->opis_city}}</p>
+</div>
 
-<h1> {{$data->name_city}}</h1>
-<p>{{$data->opis_city}}</p>
 
 </body>
 </html>
